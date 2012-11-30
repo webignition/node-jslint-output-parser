456eebf24e314c73a3beae1f92ee724f3794120f
Jon Cram <jon@webignition.net>
2012-11-30 12:08:11 +0000

    Model and parser and tests for output entry header line

---
+++ src/webignition/NodeJslintOutput/Entry/HeaderLine/HeaderLine.php
@@ -0,0 +1,73 @@
+<?php
+namespace webignition\NodeJslintOutput\Entry\HeaderLine;
+
+/**
+ * Models the header line from a nodejs-lint output entry
+ * 
+ * Raw line format:
+ * <one or more spaces>#<error number><space><error message>
+ * 
+ * Example:
+ *  #6 Expected '!==' and instead saw '!='. 
+ */
+class HeaderLine {
+    
+    /**
+     *
+     * @var int
+     */
+    private $errorNumber = null;    
+    
+    
+    /**
+     *
+     * @var string
+     */
+    private $errorMessage = null;
+    
+    
+    /**
+     * 
+     * @param int $errorNumber
+     */
+    public function setErrorNumber($errorNumber) {
+        $this->errorNumber = (int)$errorNumber;
+    }
+    
+    
+    /**
+     * 
+     * @return int
+     */
+    public function getErrorNumber() {
+        return $this->errorNumber;
+    }
+    
+    
+    /**
+     * 
+     * @param string $errorMessage
+     */
+    public function setErrorMessage($errorMessage) {
+        $this->errorMessage = $errorMessage;
+    }
+    
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getErrorMessage() {
+        return $this->errorMessage;
+    }
+    
+    
+    /**
+     * 
+     * @return string
+     */
+    public function __toString() {
+        return ' #'.$this->getErrorNumber().' '.$this->getErrorMessage();
+    }
+    
+}
---
+++ tests/NodeJslintOutput/Entry/HeaderLine/ParserTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use webignition\NodeJslintOutput\Entry\HeaderLine\Parser as HeaderLineParser;
+
+class ParserTest extends BaseTest {
+    
+    public function setUp() {
+        $this->setTestFixturePath(__CLASS__, $this->getName());
+    }       
+    
+    public function testParseErrorNumber() {
+        $parser = new HeaderLineParser();
+        
+        for ($generatedErrorNumber = 0; $generatedErrorNumber <= 100; $generatedErrorNumber++) {            
+            $parser->parse(" #".$generatedErrorNumber." Error Message Here");
+            $headerLine = $parser->getHeaderLine();
+            
+            $this->assertEquals($generatedErrorNumber, $headerLine->getErrorNumber());
+        }
+    }    
+    
+    
+    public function testParseErrorMessage() {
+        $headerLineErrorMessages = array(
+            "Unexpected '(space)'.",
+            "Combine this with the previous 'var' statement.",
+            "'$' was used before it was defined.",
+            "Expected '!==' and instead saw '!='.",
+            "Missing space between ':' and 'latestTestData'.",
+            "Expected '===' and instead saw '=='.",
+            "Move 'var' declarations to the top of the function.",
+            "Stopping.  (4% scanned).",
+        ); 
+        
+        $parser = new HeaderLineParser();
+        
+        foreach ($headerLineErrorMessages as $index => $headerLineErrorMessage) {
+            $parser->parse(' #'.$index.' '.$headerLineErrorMessage);
+            $headerLine = $parser->getHeaderLine();    
+            $this->assertEquals($headerLineErrorMessage, $headerLine->getErrorMessage());            
+        }
+    }     
+    
+}
